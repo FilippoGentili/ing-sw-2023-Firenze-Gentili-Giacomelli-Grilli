fafd095506b55546be0e13687aef3f0e5a5c4992
Marco <marcogiulio.grilli@mail.polimi.it>
2023-04-01 16:43:33 +0200

    assignpoints e player

--- MyShelfie/src/main/java/it/polimi/ingsw/Message.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Message.java
@@ -1,8 +1,28 @@
 package it.polimi.ingsw;
 
-public class Message {
-    private string text;
-    private time timestamp;
-    private id sender;
-}
+import java.util.Date;
 
+public class Message {
+
+    private Player sender;
+    private String message;
+    private Date time;
+
+    public Message(Player sender, String message){
+        this.sender = sender;
+        this.message = message;
+        this.time = new Date();
+    }
+
+    public  Player getSender(){
+        return sender;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public Date getTime(){
+        return time;
+    }
+}
