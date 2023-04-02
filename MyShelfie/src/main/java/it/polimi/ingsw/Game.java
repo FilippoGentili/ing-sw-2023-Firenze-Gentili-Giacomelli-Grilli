c77949867ff2277da077658f403aeddcaf75e21c
Marco <marcogiulio.grilli@mail.polimi.it>
2023-04-02 16:48:56 +0200

    game

--- MyShelfie/src/main/java/it/polimi/ingsw/Game.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Game.java
@@ -15,22 +15,38 @@ public class Game {
         // livingroom
     }
 
-    public Player getCurrentPlayer() {
-        return listofplayers.get(currentPlayer);
+    //process of one round
+    public static void gameLoop(){
+
     }
 
+    //returns the current player that is playing
+    public Player getCurrentPlayer() {
+        return listOfPlayers.get(currentPlayer);
+    }
+
+    //returns the next player that must play
+    public static Player getNextPlayer(){
+        return listOfPlayers.get(currentPlayer+1);
+
+    }
+
+    //return ArrayList of all participants
     public static ArrayList<Player> getPlayers() {
         return listOfPlayers;
     }
 
+    //adds player to ArrayList
     public void addPlayer(Player player) {
         this.listOfPlayers.add(player);
     }
 
+    //removes player from ArrayList
     public void removePlayer(Player player) {
         this.listOfPlayers.remove(player);
     }
 
+    //return number of tiles on living room board for different amount of players
     public int numberOfTiles() {
         int numberOfTiles;
 
@@ -51,16 +67,28 @@ public class Game {
         return numberOfTiles;
     }
 
+    //picks 2 random common goal cards
+    public pickCommonGoalCards(){
+
+    }
+
+    //picks 1 random personal goal card for each player
+    public pickPersonalGoalCard(){
+
+    }
+
+
+    //notify that it is the last round and gives the first player one extra point
     public static void endGameTrigger(Bookshelf bookshelf, @NotNull Player player) {
         int score = player.getScore();
         player.setScore(score + 1);
-
-
+        while (Game.getNextPlayer()!=player)
+            Game.gameLoop();
 
         // continua il giro finchè non arrivi al primo giocatore
     }
 
-
+    //aasigns all points to player
     public void assignPoints(@NotNull ArrayList<Player> players, PersonalGoalCard personalGoalCard, CommonGoalCard commonGoalCard){
         for (Player player : players) {
             int personalGoalPoints = personalGoalCard.assignPoints(player);
