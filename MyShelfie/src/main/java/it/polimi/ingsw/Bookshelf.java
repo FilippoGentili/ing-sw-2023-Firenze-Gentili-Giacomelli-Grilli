be7fc5ae3fe71d44f8499e775be7726ab1424af5
pheelaw <filippo1.gentili@mail.polimi.it>
2023-04-02 17:25:45 +0200

    getshelf; doing count points

--- MyShelfie/src/main/java/it/polimi/ingsw/Bookshelf.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Bookshelf.java
@@ -19,9 +19,75 @@ public class Bookshelf {
         return shelf[i][j];
     }
 
+    public Tile[][] getShelf(){
+        return shelf;
+    }
+
+    /*public ArrayList<Tile> getSameAdjacentTiles(Tile curr){
+        ArrayList<Tile> adjacents = new ArrayList<Tile>();
+        if(curr.getRow()-1 >= 0 && curr.getTileType().equals(shelf[curr.getRow()-1][curr.getCol()].getTileType()))
+            adjacents.add(shelf[curr.getRow()-1][curr.getCol()]);
+        if(curr.getCol()-1 >= 0 && curr.getTileType().equals(shelf[curr.getRow()][curr.getCol()-1].getTileType()))
+            adjacents.add(shelf[curr.getRow()][curr.getCol()-1]);
+        if(curr.getCol()+1 <= columns-1 && curr.getTileType().equals(shelf[curr.getRow()][curr.getCol()+1].getTileType()))
+            adjacents.add(shelf[curr.getRow()][curr.getCol()+1]);
+        if(curr.getRow()+1 <= rows-1 && curr.getTileType().equals(shelf[curr.getRow()+1][curr.getCol()].getTileType()))
+            adjacents.add(shelf[curr.getRow()+1][curr.getCol()]);
+
+        return adjacents;
+    }*/
+
+    public boolean hasSameAdjacentTiles(Tile curr){
+        if(curr.getRow()-1 >= 0 && curr.getTileType().equals(shelf[curr.getRow()-1][curr.getCol()].getTileType()))
+            return true;
+        if(curr.getCol()-1 >= 0 && curr.getTileType().equals(shelf[curr.getRow()][curr.getCol()-1].getTileType()))
+            return true;
+        if(curr.getCol()+1 <= columns-1 && curr.getTileType().equals(shelf[curr.getRow()][curr.getCol()+1].getTileType()))
+            return true;
+        if(curr.getRow()+1 <= rows-1 && curr.getTileType().equals(shelf[curr.getRow()+1][curr.getCol()].getTileType()))
+            return true;
+
+        return false;
+    }
+
+    public int MatrixWalk(ArrayList<Tile> tiles){
+        if(tiles.size()>0){
+            for(Tile curr:tiles){
+                MatrixWalk(curr.)
+            }
+        }
+    }
+
     public int countPoints(){
-        int curr=0;
+
+        boolean[][] alreadyChecked = new boolean[rows][columns];
         int numAd;
+        boolean more=true;
+        ArrayList<Tile> adj;
+
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                alreadyChecked[i][j]=false;
+            }
+        }
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                numAd=1;
+                alreadyChecked[i][j] = true;
+                adj = getSameAdjacentTiles(shelf[i][j]);
+                if(adj.size() > 0){
+                    for(Tile curr:adj){
+                        if(!alreadyChecked[curr.getRow()][curr.getCol()]){
+                            numAd++;
+                            getSameAdjacentTiles(shelf[curr.getRow()][curr.getCol()])
+                        }
+                    }
+                    adj = getSameAdjacentTiles(shelf[i][j]);
+                }
+            }
+        }
 
     }
 
@@ -31,6 +97,6 @@ public class Bookshelf {
                 return false;
         }
 
-        return true;    // -> chiama endgame trigger
+        return true;
     }
 }
