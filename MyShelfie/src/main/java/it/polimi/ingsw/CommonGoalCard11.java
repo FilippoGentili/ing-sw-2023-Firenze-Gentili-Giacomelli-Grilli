07f4f5848ae8f6bd65006820f698530672177b33
silvia <silvia.firenze@mail.polimi.it>
2023-04-07 11:16:57 +0200

    Silvia's commit: some changes to CommonGoalCard11Test

--- MyShelfie/src/main/java/it/polimi/ingsw/CommonGoalCard11.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/CommonGoalCard11.java
@@ -4,29 +4,21 @@ import static java.util.Objects.isNull;
 public class CommonGoalCard11 extends CommonGoalCard{
     @Override
     public boolean check(Bookshelf bookshelf) {
-        int row, col, i, j, count = 0;
+        int row, col, count;
 
-        for(row=0; row<6; row++){
-            for(col=0; col<5; col++){
-                if(!isNull(bookshelf.getTile(row,col))) {
-                    for (i = 0; i < 6; i++) {
-                        for (j = 0; j < 5; j++) {
-                            if(!isNull(bookshelf.getTile(i,j))) {
-                                if (bookshelf.getTile(row,col).getTileType() == bookshelf.getTile(i,j).getTileType())
-                                    count++;
-                            }
-                        }
+        for(TileType t: TileType.values()){
+            count=0;
+
+            for(row=0; row<6; row++){
+                for(col=0; col<5; col++){
+                    if(!isNull(bookshelf.getTile(row,col)) && bookshelf.getTile(row,col).getTileType() == t){
+                        count++;
                     }
-                    count--;
-
-                    if (count >= 8) {
-                        return true;
-                    } else {
-                        count = 0;
-                    }
-
                 }
             }
+
+            if(count>=8)  return true;
+
         }
 
         return false;
--- MyShelfie/src/test/java/it/polimi/ingsw/CommonGoalCard11Test.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/CommonGoalCard11Test.java
@@ -3,13 +3,14 @@ package it.polimi.ingsw;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import static java.util.Objects.isNull;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CommonGoalCard11Test {
     @Test
     void AllBookshelfHasOneTileType(){
-        CommonGoalCard Card = new CommonGoalCard3();
+        CommonGoalCard Card = new CommonGoalCard11();
         Tile cat = new Tile(TileType.CAT, Location.BOOKSHELF);
         ArrayList<Tile> TileCat= new ArrayList<>();
         int i,j;
@@ -26,10 +27,91 @@ class CommonGoalCard11Test {
 
     @Test
     void BookshelfStillEmpty(){
-        CommonGoalCard Card = new CommonGoalCard3();
+        CommonGoalCard Card = new CommonGoalCard11();
         Bookshelf bookshelf = new Bookshelf();
 
         assertFalse(Card.check(bookshelf));
+    }
+
+    @Test
+    void JustFirstLineFull(){
+        CommonGoalCard Card = new CommonGoalCard11();
+        Bookshelf bookshelf = new Bookshelf();
+        int i;
+
+        Tile cat = new Tile(TileType.CAT, Location.BOOKSHELF);
+        ArrayList<Tile> TileCat= new ArrayList<>();
+
+        for(i=0; i<5; i++)
+            bookshelf.insertTiles(TileCat, i);
+
+        assertFalse(Card.check(bookshelf));
+
+    }
+
+    @Test
+    void TwoRowsEquals(){
+        CommonGoalCard Card = new CommonGoalCard11();
+        Bookshelf bookshelf = new Bookshelf();
+        int i,j;
+
+        Tile game = new Tile(TileType.GAME, Location.BOOKSHELF);
+
+        ArrayList<Tile> TileGame = new ArrayList<>();
+
+        TileGame.add(game);
+
+        for(i=0; i<6; i++){
+            for(j=0; j<5; j++){
+                if(i==0 || i==1)
+                    bookshelf.insertTiles(TileGame,j);
+            }
+        }
+
+        assertTrue(Card.check(bookshelf));
+
+    }
+    @Test
+    void EachColumnIsDifferent(){
+        CommonGoalCard Card = new CommonGoalCard11();
+        Bookshelf bookshelf = new Bookshelf();
+        int i,j;
+
+        Tile cat = new Tile(TileType.CAT, Location.BOOKSHELF);
+        Tile book = new Tile(TileType.BOOK, Location.BOOKSHELF);
+        Tile trophie = new Tile(TileType.TROPHIE, Location.BOOKSHELF);
+        Tile game = new Tile(TileType.GAME, Location.BOOKSHELF);
+        Tile plant = new Tile(TileType.PLANT, Location.BOOKSHELF);
+
+        ArrayList<Tile> TileCat = new ArrayList<>();
+        ArrayList<Tile> TileBook = new ArrayList<>();
+        ArrayList<Tile> TileTrophie = new ArrayList<>();
+        ArrayList<Tile> TileGame = new ArrayList<>();
+        ArrayList<Tile> TilePlant = new ArrayList<>();
+
+        TileCat.add(cat);
+        TileBook.add(book);
+        TileTrophie.add(trophie);
+        TileGame.add(game);
+        TilePlant.add(plant);
+
+        for(i=0; i<6; i++) {
+            for (j = 0; j < 5; j++){
+                if(j==0)
+                    bookshelf.insertTiles(TileCat,j);
+                if(j==1)
+                    bookshelf.insertTiles(TileBook,j);
+                if(j==2)
+                    bookshelf.insertTiles(TileTrophie,j);
+                if(j==3)
+                    bookshelf.insertTiles(TileGame,j);
+                if(j==4)
+                    bookshelf.insertTiles(TilePlant,j);
+            }
+        }
+
+        assertFalse(Card.check(bookshelf));
+
     }
 
 }
