3b705aaf2939389376ffd99d88d36d97c8629bb4
MartinaGiacomelli <martina1.giacomelli@mail.polimi.it>
2023-04-08 16:12:49 +0200

    Merge of DrawingTile e InsertTile

--- MyShelfie/src/main/java/it/polimi/ingsw/DrawingTile.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/DrawingTile.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,15 +7,32 @@ public class DrawingTile implements State{
     private Player player;
     private static ArrayList<Tile> chosenTiles;
 
+    private ArrayList<Tile> order;
+
     int i, j;
 
     public void stateAction (){
         chosenTiles.add(player.getGame().getLivingRoom().pickTile(i, j));
         player.getGame().getLivingRoom().checkValid(chosenTiles);
 
-    }
+        //The player has to choose the column he wants to put the tiles in
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose the column");
+        int column = Integer.parseInt(scanner.nextLine());
 
-    public static ArrayList<Tile> getChosenTiles(){
-        return chosenTiles;
+        //An arraylist of tiles is created to set the order
+        order = new ArrayList<Tile>();
+        //An arraylist order is used to
+        for (Tile Tile : chosenTiles) {
+            System.out.println("Select the order of ");
+            int j = Integer.parseInt(scanner.nextLine());
+            order.add(chosenTiles.get(j));
+        }
+
+        if(!player.getBookshelf().fullBookshelf()){
+            player.getBookshelf().insertTiles(order, column);
+        }
+
+
     }
 }
