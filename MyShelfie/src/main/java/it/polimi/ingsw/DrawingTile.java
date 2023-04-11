69eee6cc1cc5db6f622abd4fede0baaaa81fccaa
MartinaGiacomelli <martina1.giacomelli@mail.polimi.it>
2023-04-11 15:50:54 +0200

    StateAction in drawing tile modificato

--- MyShelfie/src/main/java/it/polimi/ingsw/DrawingTile.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/DrawingTile.java
@@ -7,8 +7,6 @@ public class DrawingTile implements State{
     private Player player;
     private static ArrayList<Tile> chosenTiles;
 
-    private ArrayList<Tile> order;
-
     int i, j;
 
     public void stateAction (){
@@ -16,21 +14,12 @@ public class DrawingTile implements State{
         player.getGame().getLivingRoom().checkValid(chosenTiles);
 
         //The player has to choose the column he wants to put the tiles in
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Choose the column");
         int column = Integer.parseInt(scanner.nextLine());
-
-        //An arraylist of tiles is created to set the order
-        order = new ArrayList<Tile>();
-        //An arraylist order is used to
-        for (Tile Tile : chosenTiles) {
-            System.out.println("Select the order of ");
-            int j = Integer.parseInt(scanner.nextLine());
-            order.add(chosenTiles.get(j));
-        }
-
+        */
         if(!player.getBookshelf().fullBookshelf()){
-            player.getBookshelf().insertTiles(order, column);
+            player.getBookshelf().insertTiles(chosenTiles, column);
         }
 
 
