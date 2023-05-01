1e83ba2cc9ea9104550cae143694405348776057
MartinaGiacomelli <martina1.giacomelli@mail.polimi.it>
2023-05-01 17:20:08 +0200

    small update in column request structure

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/Message/ColumnRequest.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/Message/ColumnRequest.java
@@ -7,19 +7,24 @@ import java.util.ArrayList;
 public class ColumnRequest extends Message{
     private static final long serialVersionUID = 6449197724787330509L;
     private final ArrayList<Integer> availableColumns;
-    public ColumnRequest(ArrayList<Integer> availableColumns){
+    private String activePlayer;
+    public ColumnRequest(String nickname, ArrayList<Integer> availableColumns){
         super(Game.getServerName(),  MessageType.COLUMN_REQUEST);
         this.availableColumns=availableColumns;
+        this.activePlayer = nickname;
     }
 
 
     public ArrayList<Integer> getAvailableColumns(){
         return availableColumns;
     }
+    public String getActivePlayer(){
+        return activePlayer;
+    }
 
     @Override
     public String toString(){
-        return "select the column from the list: " + getAvailableColumns();
+        return " " + getActivePlayer() +" select the column from the list: " + getAvailableColumns();
     }
 
 }
