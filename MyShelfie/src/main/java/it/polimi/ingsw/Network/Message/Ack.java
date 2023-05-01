410fb29483f10f140eb38d53e612acdbf543653d
MartinaGiacomelli <martina1.giacomelli@mail.polimi.it>
2023-05-01 18:57:11 +0200

    small updates

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/Message/Ack.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/Message/Ack.java
@@ -3,10 +3,12 @@ package it.polimi.ingsw.Network.Message;
 import it.polimi.ingsw.Model.Game;
 import it.polimi.ingsw.Network.Message.*;
 
-public class Ack extends Message {
-        private static final long serialVersionUID = -2255063192863940862L;
+import java.io.Serial;
 
-        /**
+public class Ack extends Message {
+    private static final long serialVersionUID = -2255063192863940862L;
+
+    /**
          * Message used to answer the ping
          */
         public Ack() {
--- MyShelfie/src/main/java/it/polimi/ingsw/Network/Message/GivePoints.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/Message/GivePoints.java
@@ -4,7 +4,6 @@ package it.polimi.ingsw.Network.Message;
 import it.polimi.ingsw.Model.Game;
 
 public class GivePoints extends Message{
-
     private static final long serialVersionUID = 6172630014346767975L;
     private final int points;
     private final String player;
