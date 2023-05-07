8cc727966dbe0846389028861090b4640473fe43
silvia <silvia.firenze@mail.polimi.it>
2023-05-07 16:01:41 +0200

    GUI beginning3

--- MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/Scene/MenuSceneController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/Scene/MenuSceneController.java
@@ -3,9 +3,12 @@ package it.polimi.ingsw.View.Gui.Scene;
 import it.polimi.ingsw.Observer.ViewObservable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
 public class MenuSceneController extends ViewObservable {
 
+    @FXML
+    private Pane menuPane;
     @FXML
     private Button playButton;
     @FXML
