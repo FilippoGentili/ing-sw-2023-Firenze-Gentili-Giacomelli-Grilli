7a5171c1e3b8e039558d72ed2f3d7576e52c2218
Marco <marcogiulio.grilli@mail.polimi.it>
2023-05-08 20:35:39 +0200

    Network

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/Server/SocketServer.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/Server/SocketServer.java
@@ -2,8 +2,13 @@ package it.polimi.ingsw.Network.Server;
 
 import it.polimi.ingsw.Network.Message.Message;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.RemoteException;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class SocketServer {
 
@@ -14,6 +19,21 @@ public class SocketServer {
     public SocketServer(Server server, int port){
         this.server = server;
         this.port = port;
+    }
+
+    public void startSocketServer(){
+        try (ServerSocket listener = new ServerSocket(getPort())){
+            while (true){
+                try (Socket socket = listener.accept()){
+                    TimeUnit.SECONDS.sleep(20);
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                    System.out.println("Data sent");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void addClient(String nickname, MatchServer matchServer) throws RemoteException {
