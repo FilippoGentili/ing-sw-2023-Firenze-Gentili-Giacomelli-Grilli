e3561bc207f4ba2921dca90c7a2b305966a7cec7
Marco <marcogiulio.grilli@mail.polimi.it>
2023-05-10 17:46:13 +0200

    start Socket and RMI connection fix

--- MyShelfie/src/main/java/it/polimi/ingsw/ClientApp.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/ClientApp.java
@@ -10,28 +10,34 @@ public class ClientApp {
 
     public static void main(String[] args) throws IOException {
 
-        boolean cliCheck = false;
+            boolean cliCheck = false;
+            boolean guiCheck = false;
 
-        System.out.println("If you want to start a cli write '-cli' ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine().trim();
-        while(!input.equals("-cli")){
-            System.out.println("Invalid statement!");
-            System.out.println("If you want to start a cli write '-cli' ");
-            input = scanner.nextLine().trim();
+            System.out.println("How do you want to play?");
+            System.out.println("Type '-cli' or '-gui'");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine().trim();
+            while (!input.equals("-cli") && !input.equals("-gui")) {
+                System.out.println("Invalid statement!");
+                System.out.println("Type '-cli' or '-gui'");
+                input = scanner.nextLine().trim();
+            }
+
+            if (input.equals("-cli")) {
+                cliCheck = true;
+            } else if (input.equals("-gui")) {
+                guiCheck = true;
+            }
+
+            if (cliCheck) {
+                Cli view = new Cli();
+                ClientController clientController = new ClientController(view);
+                view.addObserver(clientController);
+                view.start();
+            }
+            if (guiCheck) {
+                //gui
+                System.out.println("DIO ***************");
+            }
         }
-
-        if (input.equals("-cli")) {
-            cliCheck = true;
-        }
-
-        if(cliCheck){
-            Cli view = new Cli();
-            ClientController clientController = new ClientController(view);
-            view.addObserver(clientController);
-            view.start();
-        }/*else{
-            //gui
-        }*/
-    }
 }
