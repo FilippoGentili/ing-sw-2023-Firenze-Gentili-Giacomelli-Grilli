e0e290168a0ee66473f3c1874efedeb5a3f36cd4
Marco <marcogiulio.grilli@mail.polimi.it>
2023-05-10 17:41:47 +0200

    start Socket and RMI connection fix

--- MyShelfie/src/main/java/it/polimi/ingsw/App.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,42 @@
 package it.polimi.ingsw;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import it.polimi.ingsw.Controller.ClientController;
+import it.polimi.ingsw.View.Cli;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        boolean cliCheck = false;
+        boolean guiCheck = false;
+
+        System.out.println("How do you want to play?");
+        System.out.println("Type '-cli' or 'gui'");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().trim();
+        while (!input.equals("-cli") && !input.equals("-gui")) {
+            System.out.println("Invalid statement!");
+            System.out.println("Type '-cli' or '-gui'");
+            input = scanner.nextLine().trim();
+        }
+
+        if (input.equals("-cli")) {
+            cliCheck = true;
+        } else if (input.equals("-gui")) {
+            guiCheck = true;
+        }
+
+        if (cliCheck) {
+            Cli view = new Cli();
+            ClientController clientController = new ClientController(view);
+            view.addObserver(clientController);
+            view.start();
+        }
+        if (guiCheck) {
+            //gui
+            System.out.println("DIO ***************");
+        }
     }
 }
