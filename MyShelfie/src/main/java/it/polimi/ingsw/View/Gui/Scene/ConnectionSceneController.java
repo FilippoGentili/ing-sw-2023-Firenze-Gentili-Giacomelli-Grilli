6d57e840267f33978cfca63b003548bd3e09ef6b
Marco <marcogiulio.grilli@mail.polimi.it>
2023-05-21 19:17:26 +0200

    gui fix

--- MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/Scene/ConnectionSceneController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/Scene/ConnectionSceneController.java
@@ -1,58 +1,8 @@
 package it.polimi.ingsw.View.Gui.Scene;
 
 import it.polimi.ingsw.Observer.ViewObservable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.TextField;
 
-import javafx.scene.input.MouseEvent;
+public class ConnectionSceneController extends ViewObservable {
 
-import java.io.IOException;
-
-public class ConnectionScene extends ViewObservable {
-
-    private TextField serverAddress;
-    private TextField serverPort;
-    private Button connectRMIButton;
-    private Button connectSocketButton;
-    private Hyperlink goBackLink;
-
-    /*public void setUp(){
-        connectRMIButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            try {
-                connectRMIButtonClicked();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        connectSocketButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            try {
-                connectSocketButtonClicked();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-
-        goBackLink.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> goBackLinkClicked());
-    }*/
-
-    /*private void connectRMIButtonClicked() throws IOException {
-        notifyObserver(obs -> {
-            try {
-                obs.updateServerInfoSocket();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-    }*/
-
-    private void connectSocketButtonClicked() throws IOException{
-
-    }
-
-    private void goBackLinkClicked(){
-
-    }
 
 }
